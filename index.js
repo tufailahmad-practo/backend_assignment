abb59c604e3727a1786191a1b9b28c19518f02be
tufail <tufail.ahmad@practo.com>
2026-02-04 11:58:23 +0530

    fix: deploy ready

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ app.use("/api/contacts", require("./routes/contactRoutes"));
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
